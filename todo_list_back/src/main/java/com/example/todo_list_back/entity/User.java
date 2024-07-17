12b49fbbd8ad1d13fd797743c699c3941762ecd6
Alexandre Messuve <alexandre.messuves@gmail.com>
2024-07-17 14:07:37 +0200

    maj user

--- todo_list_back/src/main/java/com/example/todo_list_back/entity/User.java
+++ todo_list_back/src/main/java/com/example/todo_list_back/entity/User.java
@@ -19,8 +19,10 @@ public class User implements UserDetails {
 
     private String name;
 
+    @Column(unique = true, nullable = false)
     private String email;
 
+    @Column(nullable = false)
     private String password;
 
     @Enumerated(EnumType.STRING)
